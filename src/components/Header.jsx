b942ea277bae61ebf633c653b06b10b4608d6b08
Mamta Kumari Vyas <mamtavyas1990@gmail.com>
2025-05-20 12:35:21 +0530

    updated Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,12 +19,6 @@ const Header = () => {
           <a href="#contact" className="hover:text-blue-600 dark:hover:text-blue-400">Contact</a>
         </nav>
 
-        {/* CTA Button */}
-        <div className="hidden md:block">
-          <button className="bg-blue-600 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-700 transition">
-            Get a Quote
-          </button>
-        </div>
       </div>
     </header>
   );
